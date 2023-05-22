68fb2ccf065bd88bf464600d376b12cb9c5f9028
Olaf Mihari <olafnjenga@gmail.com>
2023-05-22 08:25:55 +0300

    completed assignment

--- lib/car.rb
+++ lib/car.rb
@@ -1,2 +1,17 @@
 # This loads the code from the vehicle.rb file so we can access its code within this file
 require_relative './vehicle'
+
+class Car < Vehicle
+  def initialize(wheel_size, wheel_number)
+    super(wheel_size, wheel_number)
+  end
+
+  def go
+    "VRRROOOOOOOOOOOOOOOOOOOOOOOM!!!!!"
+  end
+
+  def fill_up_tank
+    "filling up!"
+  end
+end
+
